0ce286a0b2e079abae99c4a2c4eaff42e2240b0e
moises gomez <moisesg526@yahoo.com>
2024-01-06 16:46:48 -0500

    Added color to the background, a Body component, and a logo to the navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import Navbar from "./components/navbar";
+import Body from "./components/body";
+import "./css/app.css";
 
 function App() {
-  return <Navbar />;
+  return (
+    <div className="app-components">
+      <Navbar />
+      <Body />
+    </div>
+  );
 }
 
 export default App;
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,13 +1,28 @@
+import "../css/navbar.css";
+import FTD from ".././images/FTD-logo.jpg";
+
 function Navbar() {
-    return (
-        <div className="navbar">
-            <ul>
-                <li>Services</li>
-                <li>About Us</li>
-                <li>Contact Us</li>
-            </ul>
-        </div>
-    )
+  return (
+    <div className="navbar">
+      <div className="logo">
+        <img src={FTD} alt="FTD logo" height="150"/>
+      </div>
+      <ul>
+        <li>Services</li>
+        <li>About Us</li>
+        <li>Contact Us</li>
+      </ul>
+      <div className="right-nav">
+        <ul>
+          <li>Search</li>
+          <li>Cart</li>
+          <li>
+            <button>Log In</button>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default Navbar;
