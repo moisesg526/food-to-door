2c958a64a4061ba3aed2c03751afbe59114c4a70
moises gomez <moisesg526@yahoo.com>
2024-01-09 23:50:58 -0500

    Added content to body component

--- src/components/body.js
+++ src/components/body.js
@@ -1,5 +1,12 @@
+import fruitsImg from "../images/fruits.jpg";
+
 function Body() {
-    return <h1>Body</h1>
+  return (
+    <div className="body">
+      <h1>Search Nearby Eats! Click. Get it Delivered Enjoy! </h1>
+      <img src={fruitsImg} alt="fruits img" height={400}/>
+    </div>
+  );
 }
 
 export default Body;
