3edf42fb04e72591bf5f071fbab8d6c9bd04e093
moises gomez <moisesg526@yahoo.com>
2024-01-10 18:56:42 -0500

    Added content to the body component adjusted the background size and image

--- src/components/body.js
+++ src/components/body.js
@@ -1,10 +1,13 @@
-import fruitsImg from "../images/fruits.jpg";
+import "../css/body.css";
 
 function Body() {
   return (
     <div className="body">
-      <h1>Search Nearby Eats! Click. Get it Delivered Enjoy! </h1>
-      <img src={fruitsImg} alt="fruits img" height={400}/>
+      <h1>Search. Click. Get it Delivered and Enjoy! </h1>
+      <p>
+        When you hungry but feeling lazy just search for your favorite
+        restaurants and get it delivered.
+      </p>
     </div>
   );
 }
