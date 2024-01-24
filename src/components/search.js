241f8700b3bcd7c6c3e991f1a201ea94abff7b96
moises gomez <moisesg526@yahoo.com>
2024-01-23 23:58:31 -0500

    Gave design to search bar

--- src/components/search.js
+++ src/components/search.js
@@ -1,7 +1,7 @@
 import "../css/search.css";
 
 function Search() {
-  return <input type="text" placeholder="Search restaurants" />;
+  return <input className="search-bar" type="text" placeholder="Search restaurants" />;
 }
 
 export default Search;
