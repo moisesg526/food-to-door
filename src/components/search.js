febe3186337892c556cc79df34404f1ca4504ce8
moises gomez <moisesg526@yahoo.com>
2024-01-24 23:43:01 -0500

    Added spoonacular api to search component

--- src/components/search.js
+++ src/components/search.js
@@ -1,7 +1,19 @@
 import "../css/search.css";
+import React, { useState, useEffect } from "react";
+
 
 function Search() {
-  return <input className="search-bar" type="text" placeholder="Search restaurants" />;
+  const apiKey = "ea4faf2967b34be5831926bc59db3207";
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch(`https://api.spoonacular.com/food/restaurants/search?apiKey=${apiKey}`)
+    .then(res => res.json())
+    .then(json => setData(json))
+    .catch(error => console.log(error(error)));
+  }, []);
+
+  return <input className="search-bar" type="text" placeholder="Search" />;
 }
 
 export default Search;
