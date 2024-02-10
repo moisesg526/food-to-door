1f5304aa428c25d095e456ea2739067e4c01c147
moises gomez <moisesg526@yahoo.com>
2024-02-09 23:56:07 -0500

    Added Process component to body component

--- src/components/body.js
+++ src/components/body.js
@@ -1,21 +1,25 @@
 import Search from "./search";
 import "../css/body.css";
+import Process from "./process";
 
 function Body() {
   return (
-    <div className="body">
-      <div className="body-text">
-        <h1>
-          Search. Click. Get it <span className="purple">Delivered</span> and
-          Enjoy!
-        </h1>
-        <p>
-          Hungry and tired? Just search for your favorite restaurants or meals
-          and get it delivered!
-        </p>
-        <Search />
+    <>
+      <div className="body">
+        <div className="body-text">
+          <h1>
+            Search. Click. Get it <span className="purple">Delivered</span> and
+            Enjoy!
+          </h1>
+          <p>
+            Hungry and tired? Just search for your favorite restaurants or meals
+            and get it delivered!
+          </p>
+          <Search />
+        </div>
       </div>
-    </div>
+      <Process />
+    </>
   );
 }
 
