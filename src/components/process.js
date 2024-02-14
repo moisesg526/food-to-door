0697849ac74e68675e4b7031ad2cb686bb8b97f9
moises gomez <moisesg526@yahoo.com>
2024-02-13 23:50:53 -0500

    Added process css file and added a few images for the process component

--- src/components/process.js
+++ src/components/process.js
@@ -1,5 +1,33 @@
+import order from "../images/order.png";
+import delivery from "../images/delivery.png";
+import delivered from "../images/no-time.png";
+import "../css/process.css";
+
 function Process() {
-    return <h1>process</h1>
+  return (
+    <>
+    <div className="process-pitch">
+        <h2>Hungry? Easy as a click of a button.</h2>
+    </div>
+      <div className="process-animation">
+        <img
+          className="process-img"
+          src={order}
+          alt="https://storyset.com/online"
+        />
+        <img
+          className="process-img"
+          src={delivery}
+          alt="https://storyset.com/delivery"
+        />
+        <img
+          className="process-img"
+          src={delivered}
+          alt="https://storyset.com/no-time"
+        />
+      </div>
+    </>
+  );
 }
 
 export default Process;
