6ca58937ed42b8e3c689000906ea5636cc4607e2
moises gomez <moisesg526@yahoo.com>
2024-02-15 23:57:23 -0500

    Added h2 tag under images to explain the process.

--- src/components/process.js
+++ src/components/process.js
@@ -6,25 +6,34 @@ import "../css/process.css";
 function Process() {
   return (
     <>
-    <div className="process-pitch">
+      <div className="process-pitch">
         <h2>Hungry? Easy as a click of a button.</h2>
-    </div>
+      </div>
       <div className="process-animation">
-        <img
-          className="process-img"
-          src={order}
-          alt="https://storyset.com/online"
-        />
-        <img
-          className="process-img"
-          src={delivery}
-          alt="https://storyset.com/delivery"
-        />
-        <img
-          className="process-img"
-          src={delivered}
-          alt="https://storyset.com/no-time"
-        />
+        <div className="img-block">
+          <img
+            className="process-img"
+            src={order}
+            alt="https://storyset.com/online"
+          />
+          <h2>Search and click.</h2>
+        </div>
+        <div className="img-block">
+          <img
+            className="process-img"
+            src={delivery}
+            alt="https://storyset.com/delivery"
+          />
+          <h2>Our service is fast a reliable.</h2>
+        </div>
+        <div className="img-bloc">
+          <img
+            className="process-img"
+            src={delivered}
+            alt="https://storyset.com/no-time"
+          />
+          <h2>Get your order and enjoy!</h2>
+        </div>
       </div>
     </>
   );
